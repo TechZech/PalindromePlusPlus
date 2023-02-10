76e555e11bead3534f784018c9316c1fa82d6ce6
Ben Zech <72528903+TechZech@users.noreply.github.com>
2023-02-09 22:00:48 -0500

    Started work on compiler

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+bool isPalindrome(string line);
+string splitPalindrome(string palindrome);
+
+int main(int argc, char** argv)
+{
+    string filename = argv[1];
+    ifstream file(filename);
+    string line;
+    ofstream outfile(filename + ".out");
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+//            outfile << 'a' + line + '\n';
+
+        }
+        file.close();
+        outfile.close();
+    } else {
+        cout << "Error: Unable to open " << filename << endl;
+    }
+
     return 0;
+}
+
+// Returns if the line is a valid palindrome
+bool isPalindrome(string line) {
+
+}
+
+// Returns split palindrome
+string splitPalindrome(string palindrome) {
+
 }
